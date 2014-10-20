04d859d499e22ed1f160b8995a70685d3ab00e21
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-10-20 13:06:59 +0200

    Multiclient setup

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ var util = require('util');
 var pcap = require('pcap');
 
 var session = pcap.createSession('', 'tcp');
+var clients = [];
 
 wss.on('connection', function(ws) {
-	session.on('packet', function(raw_packet) {
-		var packet = pcap.decode.packet(raw_packet);
-    	ws.send(JSON.stringify(packet));
-	});
+	clients.push(ws);
+
+	ws.on('close', function() {
+	    clients.splice(clients.indexOf(ws), 1);
+	});	
 });
+
+session.on('packet', function(raw_packet) {
+	var packet = pcap.decode.packet(raw_packet);
+
+	for (client in clients)
+		clients[client].send(JSON.stringify(packet));
+});
+
+setInterval(function(){
+	console.log("Connected clients: ", clients.length)
+}, 500)
