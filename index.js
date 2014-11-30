82020da2f3310ade0d9d20d0a531ce7e4b2ee046
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-11-30 14:49:54 +0100

    IP Address from server to client

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ var pcap = require('pcap');
 
 var session = pcap.createSession('', 'tcp');
 var clients = [];
+var my_ips = get_ip_addresses(session);
 
 wss.on('connection', function(ws) {
+	// First send the client the IP address of the server
+	ws.send(JSON.stringify({"type": "ip", "ip": my_ips}));
+
 	clients.push(ws);
-	logClients();
+	log_clients();
 
 	ws.on('close', function() {
 	    clients.splice(clients.indexOf(ws), 1);
-	    logClients();
+	    log_clients();
 	});	
 });
 
@@ -22,9 +26,24 @@ session.on('packet', function(raw_packet) {
 	var packet = pcap.decode.packet(raw_packet);
 
 	for (client in clients)
-		clients[client].send(JSON.stringify(packet));
+		clients[client].send(JSON.stringify({"type": "packet", "packet": packet}));
 });
 
-function logClients() {
+function log_clients() {
 	console.log("Connected clients: ", clients.length);
 }
+
+function get_ip_addresses(session) {
+	res = [];
+
+	session
+		// Find all the interfaces
+		.findalldevs()
+		// Get adresses of the active one
+		.filter(function(all_devs) { return session.device_name === all_devs.name; })[0].addresses
+		.forEach(function(e){ res.push(e.addr) })
+
+	return res;
+}
+
+
