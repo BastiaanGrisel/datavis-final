706357f3b9571be623d1be21b30f31143f1c392f
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-02-09 21:09:59 +0100

    IPs to countries

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var WebSocketServer = require('ws').Server,
  	pcap 	= require('pcap'),
 	swig 	= require("swig"),
 	http 	= require("http"),
-	geo_reader = require('maxmind-db-reader');
-	
+	geo_reader = require('maxmind-db-reader'),
+	getIP = require('external-ip')();
 
 var session = pcap.createSession('', 'tcp'),
 	geolocation = geo_reader.openSync('./public/data/GeoLite2-Country.mmdb'),
@@ -13,12 +13,22 @@ var session = pcap.createSession('', 'tcp'),
 	server 	= http.createServer(router),
 	wss 	= new WebSocketServer({port: 8080}),
 	clients = [],
-	my_ips  = get_ip_addresses(session);
+	internal_ips = get_ip_addresses(session),
+	external_ip = null;
+
+getIP(function (err, ip) {
+    if (err) {
+        // every service in the list has failed 
+        throw err;
+    }
+    
+    external_ip = ip;
+});
 
 // Websocket server code
 wss.on('connection', function(ws) {
 	// First send the client the IP address(es) of the server
-	ws.send(JSON.stringify({"type": "ip", "ip": my_ips}));
+	ws.send(JSON.stringify({"type": "ip", "ip": external_ip}));
 
 	clients.push(ws);
 	log_clients(); // Display the number of connected clients in the terminal
@@ -36,9 +46,17 @@ session.on('packet', function(raw_packet) {
 	var daddr = packet.payload.payload.daddr.toString();
 	var size  = packet.pcap_header.len;
 
+	if(internal_ips.indexOf(saddr) != -1) {
+		saddr = external_ip;
+	}
+
+	if(internal_ips.indexOf(daddr) != -1) {
+		daddr = external_ip;
+	}
+
 	// Dispatch the packet to all connected clients
 	for (client in clients)
-		clients[client].send(JSON.stringify({"type": "packet", "packet": {"saddr": saddr, "daddr": daddr, "size": size}}));
+		clients[client].send(JSON.stringify({"type": "packet", "packet": {"sloc": geolocation.getGeoDataSync(saddr), "dloc": geolocation.getGeoDataSync(daddr), "saddr": saddr, "daddr": daddr, "size": size}}));
 });
 
 function log_clients() {
@@ -53,7 +71,7 @@ function get_ip_addresses(session) {
 		.findalldevs()
 		// Get adresses of the active one
 		.filter(function(all_devs) { return session.device_name === all_devs.name; })[0].addresses
-		.forEach(function(e){ res.push(e.addr); res.push(geolocation.getGeoDataSync(e.addr)); })
+		.forEach(function(e){ res.push(e.addr); })
 
 	return res;
 }
