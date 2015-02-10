8556cffb785519aad7063ef60077e4cf3de13db9
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-02-10 14:12:07 +0100

    Added iso code to server

--- server.js
+++ server.js
@@ -23,40 +23,49 @@ getIP(function (err, ip) {
     }
     
     external_ip = ip;
-});
 
-// Websocket server code
-wss.on('connection', function(ws) {
-	// First send the client the IP address(es) of the server
-	ws.send(JSON.stringify({"type": "ip", "ip": external_ip}));
+    // Websocket server code
+	wss.on('connection', function(ws) {
+		// First send the client the IP address(es) of the server
+		ws.send(JSON.stringify({"type": "ip", "ip": external_ip}));
 
-	clients.push(ws);
-	log_clients(); // Display the number of connected clients in the terminal
+		clients.push(ws);
+		log_clients(); // Display the number of connected clients in the terminal
 
-	ws.on('close', function() {
-	    clients.splice(clients.indexOf(ws), 1);
-	    log_clients();
-	});	
-});
+		ws.on('close', function() {
+		    clients.splice(clients.indexOf(ws), 1);
+		    log_clients();
+		});	
+	});
 
-session.on('packet', function(raw_packet) {
-	var packet = pcap.decode.packet(raw_packet);
+	session.on('packet', function(raw_packet) {
+		var packet = pcap.decode.packet(raw_packet);
 
-	var saddr = packet.payload.payload.saddr.toString();
-	var daddr = packet.payload.payload.daddr.toString();
-	var size  = packet.pcap_header.len;
+		var saddr = packet.payload.payload.saddr.toString();
+		var daddr = packet.payload.payload.daddr.toString();
+		var size  = packet.pcap_header.len;
 
-	if(internal_ips.indexOf(saddr) != -1) {
-		saddr = external_ip;
-	}
+		if(internal_ips.indexOf(saddr) != -1) {
+			saddr = external_ip;
+		}
 
-	if(internal_ips.indexOf(daddr) != -1) {
-		daddr = external_ip;
-	}
+		if(internal_ips.indexOf(daddr) != -1) {
+			daddr = external_ip;
+		}
 
-	// Dispatch the packet to all connected clients
-	for (client in clients)
-		clients[client].send(JSON.stringify({"type": "packet", "packet": {"sloc": geolocation.getGeoDataSync(saddr), "dloc": geolocation.getGeoDataSync(daddr), "saddr": saddr, "daddr": daddr, "size": size}}));
+		try {
+			saddr = geolocation.getGeoDataSync(saddr).country.iso_code;
+			daddr = geolocation.getGeoDataSync(daddr).country.iso_code;
+
+			// Dispatch the packet to all connected clients
+			for (client in clients)
+				clients[client].send(JSON.stringify({"type": "packet", "packet": {"sloc": saddr, "dloc": daddr, "size": size}}));
+		} catch(err) {
+			console.log("No country found ("+err+")");
+		}
+	});
+
+    server.listen(80);
 });
 
 function log_clients() {
@@ -85,4 +94,3 @@ router.get("/", function(request, response) {
 	response.end();
 });
 
-server.listen(80);
